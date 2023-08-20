a29fbd29833fc5fef590e6520655c5fa7504a3cd
Beem Media <beemmediasoftware@gmail.com>
2023-08-19 20:32:25 -0500

    [HWD3D] Clean up WinMain a bit.

--- _Common/Shell/HWD3DWinMain.cpp
+++ _Common/Shell/HWD3DWinMain.cpp
@@ -8,46 +8,26 @@ static const WCHAR WinMain_Title[] = L"Hello World D3D";
 static const WCHAR WinMain_WindowClass[] = L"HelloWorldD3D";
 
 static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+static void HWD3DWinMain_SetDPIAware();
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-#if 1//(_WIN32_WINNT >= 0x0600)
-	{
+	HWD3DWinMain_SetDPIAware();
 
-#if 1
-		// Load and call SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE) directly from DLL.
-		HMODULE WinUserLib = LoadLibraryW(L"User32.dll");
-		if (WinUserLib != INVALID_HANDLE_VALUE)
-		{
-			typedef BOOL ( WINAPI * FnType)(_In_ HANDLE value);
-			FnType Fn = reinterpret_cast<FnType>(GetProcAddress(WinUserLib, "SetProcessDpiAwarenessContext"));
-			if (Fn)
-			{
-				Fn(((HANDLE)-3));
-			}
-			FreeLibrary(WinUserLib);
-		}
-#else
-		SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
-#endif
-	}
-#endif
-
-	const POINT DisplayRes = { 800 , 600 }; // TODO: Command Line Parameter
-	const float GameUpdateRate = 1.f/60.f;
+	const POINT DisplayRes = { 800, 600 }; // TODO: Command Line Parameter
+	const float GameUpdateRate = 1.f / 60.f;
 	const int MaxUpatesPerFrameThreshold = 10;
 
 	HWND MainWnd = NULL;
-	RECT MainWnd_ClientRect = { };
 
 	// Register Window Class:
 	{
 		WNDCLASSEXW wcex = { };
 		wcex.cbSize = sizeof(WNDCLASSEX);
-		wcex.style = CS_HREDRAW|CS_VREDRAW;
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
 		wcex.lpfnWndProc = WndProc;
 		wcex.cbClsExtra = 0;
 		wcex.cbWndExtra = 0;
@@ -68,56 +48,62 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
 	// Create Main Window
 	{
-		MainWnd = CreateWindowW(WinMain_WindowClass, WinMain_Title, WS_CAPTION|WS_SYSMENU|WS_MINIMIZEBOX, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+		MainWnd = CreateWindowW(WinMain_WindowClass, WinMain_Title, WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
 		if (!MainWnd)
 		{
 			return -1;
 		}
+	}
 
-		// Adjust window to be our resolution size:
-		RECT RcSize = { 0 , 0 , DisplayRes.x , DisplayRes.y };
-		const LONG Style = GetWindowLongW( MainWnd , GWL_STYLE );
-		const LONG ExStyle = GetWindowLongW( MainWnd , GWL_EXSTYLE );
-		AdjustWindowRectEx( &RcSize , Style , TRUE , ExStyle );
-		SetWindowPos( MainWnd , NULL , 0 , 0 , RcSize.right - RcSize.left , RcSize.bottom - RcSize.top , SWP_NOREPOSITION|SWP_NOMOVE );
-		GetClientRect( MainWnd , &MainWnd_ClientRect );
+	// Adjust window to be our resolution size:
+	{
+		RECT RcSize = { 0, 0, DisplayRes.x, DisplayRes.y };
+		const LONG Style = GetWindowLongW(MainWnd, GWL_STYLE);
+		const LONG ExStyle = GetWindowLongW(MainWnd, GWL_EXSTYLE);
+		AdjustWindowRectEx(&RcSize, Style, TRUE, ExStyle);
+		SetWindowPos(MainWnd, NULL, 0, 0, RcSize.right - RcSize.left, RcSize.bottom - RcSize.top, SWP_NOREPOSITION | SWP_NOMOVE);
 
 		ShowWindow(MainWnd, nCmdShow);
 		UpdateWindow(MainWnd);
 	}
 
 	HWD3DGame* Game = HWD3DGame::CreateGame(MainWnd);
+	if (!Game)
+	{
+		DestroyWindow(MainWnd);
+		return -1;
+	}
+
 	bool bGameStillRunning = true;
 
-	LARGE_INTEGER PerfFreq = { };
-	QueryPerformanceFrequency( &PerfFreq );
-	auto GetRawTimeElapsedSec =[&PerfFreq]( const LARGE_INTEGER& Start , const LARGE_INTEGER& End ) -> float
-	{
-		assert( Start.QuadPart <= End.QuadPart );
-		const LONGLONG Diff = End.QuadPart - Start.QuadPart;
-		const double TimeMs = (Diff * 1000.) / PerfFreq.QuadPart;
-		const float TimeSec = static_cast<float>(TimeMs) / 1000.f;
-		return TimeSec;
-	};
-
-	LARGE_INTEGER FrameStartTime = { };
-	LARGE_INTEGER FrameEndTime = { };
-	QueryPerformanceCounter( &FrameStartTime );
+	LARGE_INTEGER PerfFreq = {};
+	QueryPerformanceFrequency(&PerfFreq);
+	auto GetRawTimeElapsedSec = [&PerfFreq](const LARGE_INTEGER& Start, const LARGE_INTEGER& End) -> float
+		{
+			assert(Start.QuadPart <= End.QuadPart);
+			const LONGLONG Diff = End.QuadPart - Start.QuadPart;
+			const double TimeMs = (Diff * 1000.) / PerfFreq.QuadPart;
+			const float TimeSec = static_cast<float>(TimeMs) / 1000.f;
+			return TimeSec;
+		};
+	
+	LARGE_INTEGER FrameStartTime = {};
+	LARGE_INTEGER FrameEndTime = {};
+	QueryPerformanceCounter(&FrameStartTime);
 	FrameEndTime = FrameStartTime;
 	float CumulativeDelta = 0.f;
 
 	while (bGameStillRunning)
 	{
-		const float GameDeltaTime = GetRawTimeElapsedSec( FrameStartTime , FrameEndTime );
+		const float GameDeltaTime = GetRawTimeElapsedSec(FrameStartTime, FrameEndTime);
 		const float TotalDelta = GameDeltaTime + CumulativeDelta;
 		CumulativeDelta = TotalDelta;
 
 		// Pump Messages
-		MSG msg = { };
-		while (PeekMessageW(&msg, nullptr, 0 , 0, PM_NOREMOVE))
+		MSG msg = {};
+		while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
-			GetMessage(&msg, nullptr, 0, 0);
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 
@@ -130,13 +116,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 		if (bGameStillRunning)
 		{
 			int NumUpdatesThisFrame = 0;
-			while( CumulativeDelta >= GameUpdateRate )
+			while (CumulativeDelta >= GameUpdateRate)
 			{
 				Game->Update(GameUpdateRate);
 				CumulativeDelta -= GameUpdateRate;
 				NumUpdatesThisFrame++;
 
-				if( NumUpdatesThisFrame >= MaxUpatesPerFrameThreshold )
+				if (NumUpdatesThisFrame >= MaxUpatesPerFrameThreshold)
 				{
 					CumulativeDelta = 0.f;
 					break;
@@ -146,7 +132,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 		}
 
 		FrameStartTime = FrameEndTime;
-		QueryPerformanceCounter( &FrameEndTime );
+		QueryPerformanceCounter(&FrameEndTime);
 	}
 
 	HWD3D_SafeRelease(Game);
@@ -160,7 +146,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 	{
 	case WM_COMMAND:
 	{
-		int wmId = LOWORD(wParam);
+		const int wmId = LOWORD(wParam);
 		// Parse the menu selections:
 		switch (wmId)
 		{
@@ -172,13 +158,6 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 		}
 	}
 	break;
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		EndPaint(hWnd, &ps);
-	}
-	break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -186,4 +165,30 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
+}
+
+static void HWD3DWinMain_SetDPIAware()
+{
+#define HWD3DWINMAIN_CALL_DLL_DPI_AWARENESS 1
+
+#if HWD3DWINMAIN_CALL_DLL_DPI_AWARENESS
+	// Load and call SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE) directly from DLL.
+	// Since we're getting this function from the DLL, if it doesn't exist on this version of Windows, it
+	// simply won't be called.
+	HMODULE WinUserLib = LoadLibraryW(L"User32.dll");
+	if (WinUserLib)
+	{
+		typedef BOOL(WINAPI* FnType)(_In_ HANDLE value);
+		FnType Fn = reinterpret_cast<FnType>(GetProcAddress(WinUserLib, "SetProcessDpiAwarenessContext"));
+		if (Fn)
+		{
+			Fn(((HANDLE)-3));
+		}
+		FreeLibrary(WinUserLib);
+	}
+#else
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+#endif
+
+#undef HWD3DWINMAIN_CALL_DLL_DPI_AWARENESS
 }
